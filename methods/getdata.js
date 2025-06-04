3874eb104537c3d2d7f7937775fce5c7b871545c
Liverbreaker <willy083092@gmail.com>
2025-06-05 00:46:53 +0800

    Init

---
+++ methods/getdata.js
@@ -0,0 +1,38 @@
+const updateData=(fetch_URL, weather)=>{
+	fetch(`${fetch_URL}`, { mode: 'cors', credentials: 'include'})
+	.then(resp=>weather =resp.json())
+	.catch(err => console.error(err));
+	console.log("Success!!")
+};
+
+const updateRegularly = (schedule, fetch_URL, weather)=>{
+	// Schedule a job at 5:30 AM
+	schedule.scheduleJob('30 5 * * *', function() {
+	    console.log('Executing task at 5:30 AM');
+	    updateData();
+	});
+
+	// Schedule a job at 11:30 AM
+	schedule.scheduleJob('30 11 * * *', function() {
+	    console.log('Executing task at 11:30 AM');
+	    updateData();
+	});
+
+	// Schedule a job at 5:30 PM
+	schedule.scheduleJob('30 17 * * *', function() {
+	    console.log('Executing task at 5:30 PM');
+	    updateData();
+	});
+	// Schedule a job at 11:30 PM
+	schedule.scheduleJob('30 23 * * *', function() {
+	    console.log('Executing task at 11:30 PM');
+	    updateData();
+	});
+};
+
+
+
+module.exports={
+	updateRegularly,
+	updateData
+};
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors'); 
+const schedule = require('node-schedule');
+// const dotenv = require('dotenv');
+const data = require('./methods/getdata');
+
+// require('dotenv').config()
+async function getWeather(){
+	const res = await fetch(`${fetch_URL}`, { mode: 'cors', credentials: 'include'})
+	.catch(err => console.error(err));
+	weather = await res.json();
+};
+
+
+let weather;
+const fetch_URL = process.env.FetchURL ;
+const port = process.env.PORT || 4000;
+
+getWeather();
+data.updateRegularly(schedule, fetch_URL, weather);
+
+const app = express();
+// app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/',(req, resp) =>{
+	resp.send(`This is working!!`);
+})
+app.get('/getinfo', (req, resp) =>{
+	resp.json(weather);
+})
+
+app.listen(port,()=> {
+	console.log(`Server is running on port ${port}`);
+});
